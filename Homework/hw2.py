7d9c8913789558dc8f49587a5f5795414a746166
Sean O <seanokeefe0314@gmail.com>
2013-10-03 19:21:50 +1000

    hw2 answers

--- Homework/hw2.py
+++ Homework/hw2.py
@@ -4,16 +4,15 @@ This source code is released under the New BSD license.  Please see
 http://wiki.quantsoftware.org/index.php?title=QSTK_License
 for license details.
 
-Created on March, 5, 2012
+Created on January, 23, 2013
 
 @author: Sourabh Bajaj
-@contact: sourabhbajaj90@gmail.com
+@contact: sourabhbajaj@gatech.edu
 @summary: Event Profiler Tutorial
 '''
 
 
-import pandas 
-from QSTK.qstkutil import DataAccess as da
+import pandas as pd
 import numpy as np
 import math
 import copy
@@ -21,6 +20,7 @@ import QSTK.qstkutil.qsdateutil as du
 import datetime as dt
 import QSTK.qstkutil.DataAccess as da
 import QSTK.qstkutil.tsutil as tsu
+import csv
 import QSTK.qstkstudy.EventProfiler as ep
 
 """
@@ -39,83 +39,54 @@ nan = no information about any event.
 1 = status bit(positively confirms the event occurence)
 """
 
-# Get the data from the data store
-storename = "Yahoo" # get data from our daily prices source
-# Available field names: open, close, high, low, close, actual_close, volume
-closefield = "actual_close"
-volumefield = "volume"
-window = 10
 
-def findEvents(symbols, startday,endday, marketSymbol,verbose=False):
+def find_events(ls_symbols, d_data):
+    ''' Finding the event dataframe '''
+    df_close = d_data['actual_close']
+    ts_market = df_close['SPY']
 
-	# Reading the Data for the list of Symbols.	
-	timeofday=dt.timedelta(hours=16)
-	timestamps = du.getNYSEdays(startday,endday,timeofday)
-	dataobj = da.DataAccess('Yahoo')
-	if verbose:
-            print __name__ + " reading data"
-	# Reading the Data
-	close = dataobj.get_data(timestamps, symbols, closefield)
-	
-	# Completing the Data - Removing the NaN values from the Matrix
-#	close = (close.fillna(method='ffill')).fillna(method='backfill')
+    print "Finding Events"
 
-	
-	# Calculating Daily Returns for the Market
-#!!!	tsu.returnize0(close.values)
-#	SPYValues=close[marketSymbol]
+    # Creating an empty dataframe
+    df_events = copy.deepcopy(df_close)
+    df_events = df_events * np.NAN
 
-	# Calculating the Returns of the Stock Relative to the Market 
-	# So if a Stock went up 5% and the Market rised 3%. The the return relative to market is 2% 
-#	mktneutDM = close - close[marketSymbol]
-	np_eventmat = copy.deepcopy(close)
-	for sym in symbols:
-		for time in timestamps:
-			np_eventmat[sym][time]=np.NAN
+    # Time stamps for the event range
+    ldt_timestamps = df_close.index
 
-	if verbose:
-            print __name__ + " finding events"
+    for s_sym in ls_symbols:
+        for i in range(1, len(ldt_timestamps)):
+            # Calculating the returns for this timestamp
+            f_symprice_today = df_close[s_sym].ix[ldt_timestamps[i]]
+            f_symprice_yest = df_close[s_sym].ix[ldt_timestamps[i - 1]]
+            f_marketprice_today = ts_market.ix[ldt_timestamps[i]]
+            f_marketprice_yest = ts_market.ix[ldt_timestamps[i - 1]]
+            f_cutoff = 5.0
+            if f_symprice_today < f_cutoff and f_symprice_yest >= f_cutoff:
+                df_events[s_sym].ix[ldt_timestamps[i]] = 1
 
-	# Generating the Event Matrix
-	# Event described is : Market falls more than 3% plus the stock falls 5% more than the Market
-	# Suppose : The market fell 3%, then the stock should fall more than 8% to mark the event.
-	# And if the market falls 5%, then the stock should fall more than 10% to mark the event.
-
-	for symbol in symbols:
-		
-	    for i in range(1,len(close[symbol])):
-	        if close[symbol][timestamps[i-1]] >= 7.0 and close[symbol][timestamps[i]] < 7.0 :
-             		np_eventmat[symbol][i] = 1.0  #overwriting by the bit, marking the event
-
-#	        if SPYValues[i]<-0.03 and mktneutDM[symbol][i] < -0.05 : # When
-                        # market fall is more than 3% and also the stock
-                        # compared to market is also fell by more than 5%.
-			
-	return np_eventmat
+    return df_events
 
 
-#################################################
-################ MAIN CODE ######################
-#################################################
+if __name__ == '__main__':
+    dt_start = dt.datetime(2008, 1, 1)
+    dt_end = dt.datetime(2009, 12, 31)
+    ldt_timestamps = du.getNYSEdays(dt_start, dt_end, dt.timedelta(hours=16))
 
+    dataobj = da.DataAccess('Yahoo')
+    ls_symbols = dataobj.get_symbols_from_list('sp5002012')
+    ls_symbols.append('SPY')
+    ls_keys = ['open', 'high', 'low', 'close', 'volume', 'actual_close']
+    ldf_data = dataobj.get_data(ldt_timestamps, ls_symbols, ls_keys)
+    d_data = dict(zip(ls_keys, ldf_data))
 
-# symbols = np.loadtxt('SP500port.csv',dtype='S10',comments='#', skiprows=1)
-# You might get a message about some files being missing, don't worry about it.
+    for s_key in ls_keys:
+        d_data[s_key] = d_data[s_key].fillna(method = 'ffill')
+        d_data[s_key] = d_data[s_key].fillna(method = 'bfill')
+        d_data[s_key] = d_data[s_key].fillna(1.0)
 
-dataobj = da.DataAccess('Yahoo')
-symbols = dataobj.get_symbols_from_list("sp5002012")
-symbols.append('SPY')
-
-#symbols =['BFRE','ATCS','RSERF','GDNEF','LAST','ATTUF','JBFCF','CYVA','SPF','XPO','EHECF','TEMO','AOLS','CSNT','REMI','GLRP','AIFLY','BEE','DJRT','CHSTF','AICAF']
-startday = dt.datetime(2008,1,1)
-#!!! startday = dt.datetime(2008,1,1)
-endday = dt.datetime(2009,12,31)
-#!!! endday = dt.datetime(2010,12,31)
-
-eventMatrix = findEvents(symbols,startday,endday,marketSymbol='SPY',verbose=True)
-
-eventProfiler = ep.EventProfiler(eventMatrix,startday,endday,lookback_days=20,lookforward_days=20,verbose=True)
-
-eventProfiler.study(filename="MyEventStudyHW.pdf",plotErrorBars=True,plotMarketNeutral=True,plotEvents=False,marketSymbol='SPY')
-
-
+    df_events = find_events(ls_symbols, d_data)
+    print "Creating Study"
+    ep.eventprofiler(df_events, d_data, i_lookback=20, i_lookforward=20,
+                 s_filename='MyEventStudy.pdf', b_market_neutral=True, b_errorbars=True,
+                s_market_sym='SPY')
